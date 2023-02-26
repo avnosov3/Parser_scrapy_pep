c65e2a0a3a334424eb4dfb9eff8cc92242a1a187
Artem Nosov <nosov1995@gmail.com>
2023-02-26 14:55:25 +0100

    3 try

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,33 +1,25 @@
+from collections import defaultdict
 import csv
 import datetime as dt
 
-from scrapy.exceptions import DropItem
 
 from pep_parse.settings import BASE_DIR, RESULTS
 
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 FILE_NAME = 'status_summary_{}.csv'
-DUBLICATE_MESSAGE = 'Найдена повторяющееся запись: "{}"'
 
 
 class PepParsePipeline:
 
     def __init__(self):
-        self.numbers_seen = set()
         self.results_dir = BASE_DIR / RESULTS
         self.results_dir.mkdir(exist_ok=True)
-        self.statuses = {}
 
     def open_spider(self, spider):
-        pass
+        self.statuses = defaultdict(int)
 
     def process_item(self, item, spider):
-        number = item['number']
-        status = item['status']
-        if number in self.numbers_seen:
-            raise DropItem(DUBLICATE_MESSAGE.format(item))
-        self.numbers_seen.add(number)
-        self.statuses[status] = self.statuses.get(status, 0) + 1
+        self.statuses[item['status']] += 1
         return item
 
     def close_spider(self, spider):
